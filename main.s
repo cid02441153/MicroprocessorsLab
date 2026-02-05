9985af36e3e1f268c9b2691042380a054a106198
gp723 <gp723@skbl-418-010.ph.ic.ac.uk>
2026-02-05 11:09:48 +0000

    Added comments to learn about assembly code

--- main.s
+++ main.s
@@ -8,23 +8,23 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
+	movlw 0xFF
+	movwf TRISD, A		    ; Port D all inputs
+	
 	movlw 	0x0
 	movwf	TRISC, A	    ; Port C all outputs
 	
-	movlw 0x01
-	movwf TRISD, A		    ; Port D all inputs
-	
 	bra 	test
 loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
+	movff 	0x06, PORTC ; Move value from register 6 to Port C
+	incf 	0x06, W, A ; Increment value of register 6, and store in W
 test:
 	
 	movwf	0x06, A	    ; Test for end of loop condition
 	
-	movf PORTD, W, A
+	movf PORTD, W, A    ; Read in value from Port D, store in Access
 	
-	cpfsgt 	0x06, A
+	cpfsgt 	0x06, A	    ; Compares register 6 to what is stored in W
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
 
