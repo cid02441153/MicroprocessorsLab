3b412eadb069d346ee65698e4c0f1b85614530e7
Peters <gp723@ic.ac.uk>
2026-02-19 11:23:14 +0000

    Combine motor spinnin with gyro measurement

--- main.s
+++ main.s
@@ -9,6 +9,8 @@ ACCEL_X_H: ds 1
 POS_H:     ds 1 
 POS_L:     ds 1 
 TEMP:      ds 1
+MAGNITUDE: ds 1
+SKIP_CNT:  ds 1 ; Counter for pulse skipping
 
 psect code, abs
 main:
@@ -19,62 +21,82 @@ setup:
     banksel OSCCON
     movlw 0x70
     movwf OSCCON, b      ; 16MHz
-    
-    ; 1. Digital Setup
     banksel ANCON3
     clrf ANCON3, b       
-
     banksel TRISD
     bcf TRISD, 2, b      ; RD2 -> SERVO
     bcf TRISD, 0, b      ; CS
     bcf TRISD, 6, b      ; SCK
     bcf TRISD, 4, b      ; SDO
     bsf TRISD, 5, b      ; SDI
-
     banksel TRISE
-    clrf TRISE, b        ; LEDs Output
-    banksel LATE
-    movlw 0xAA           ; Pattern to show PIC is alive
-    movwf LATE, b
-
+    clrf TRISE, b
     banksel LATD
     bsf LATD, 0, b       ; CS High
-    
-    ; 2. SPI Setup
     banksel SSP2CON1
-    clrf SSP2CON1, b
-    movlw 0x32           ; SPI Enable
+    movlw 0x32           
     movwf SSP2CON1, b
-
-    ; 3. Wait for Sensor to power up
+    
+    clrf SKIP_CNT, c
     call Delay_Rest
     call LSM9DS1_Wakeup
 
 loop:
-    ; 4. Read Sensor
     call LSM9DS1_ReadX
 
-    ; 5. Show Data on LEDs
+    ; 1. SHOW RAW DATA ON LEDS
     banksel LATE
     movf ACCEL_X_H, W, c
     movwf LATE, b
 
-    ; 6. Calculate Pulse (Target 1.5ms = 1000 Ticks)
-    ; If the motor spins constantly, we might be sending too many ticks.
-    ; Let's use a conservative 1.5ms center.
-    movlw 0xE8           
-    movwf POS_L, c
-    movlw 0x03           
-    movwf POS_H, c
-
+    ; 2. MAGNITUDE & DEADZONE
     movf ACCEL_X_H, W, c
-    movwf TEMP, c
+    movwf MAGNITUDE, c
+    btfsc MAGNITUDE, 7, c
+    negf MAGNITUDE, c    
+
+    movlw 4
+    cpfslt MAGNITUDE, c  
+    bra Pulse_Logic
+    bra loop             ; Board flat = No signal
+
+Pulse_Logic:
+    ; 3. PULSE SKIPPING (The Speed Secret)
+    ; We only send a pulse if SKIP_CNT >= (20 - MAGNITUDE)
+    ; Large magnitude = Smaller wait = Faster speed
+    incf SKIP_CNT, f, c
     
-    tstfsz TEMP, c
-    call Apply_Gain
+    movf MAGNITUDE, W, c
+    sublw 20             ; Max wait of 20 cycles
+    cpfslt SKIP_CNT, c
+    bra Send_Pulse       ; Time to move!
+    
+    call Delay_Rest      ; Just wait, don't pulse
+    bra loop
 
 Send_Pulse:
-    ; 7. PWM (RD2)
+    clrf SKIP_CNT, c     ; Reset skip counter
+    
+    ; 4. DIRECTIONAL PULSE
+    ; We send a fixed "Step" pulse (1.0ms or 2.0ms)
+    ; Since frequency handles speed, we don't need variable width.
+    btfss ACCEL_X_H, 7, c
+    bra Go_Right
+
+Go_Left:
+    movlw 0xE8           ; 1.0ms
+    movwf POS_L, c
+    movlw 0x03
+    movwf POS_H, c
+    bra Execute
+
+Go_Right:
+    movlw 0xD0           ; 2.0ms
+    movwf POS_L, c
+    movlw 0x07
+    movwf POS_H, c
+
+Execute:
     banksel LATD
     bsf LATD, 2, b       
     call Delay_us        
@@ -84,45 +106,18 @@ Send_Pulse:
     call Delay_Rest      
     bra loop
 
-Apply_Gain:
-    ; Gain x16 (Quick Swap)
-    ; This is safer than x64 for a first test
-    swapf TEMP, W, c
-    andlw 0xF0
-    movwf TEMP, c
-    
-    btfss ACCEL_X_H, 7, c
-    bra Pos_Move
-
-Neg_Move:
-    ; Absolute value of swapped temp
-    comf TEMP, f, c
-    incf TEMP, f, c
-    movf TEMP, W, c
-    subwf POS_L, f, c
-    btfss STATUS, 0, c
-    decf POS_H, f, c
-    return
-
-Pos_Move:
-    movf TEMP, W, c
-    addwf POS_L, f, c
-    btfsc STATUS, 0, c
-    incf POS_H, f, c
-    return
-
+; --- Support ---
 Delay_us:
     movff POS_L, D1
     movff POS_H, D2
-L_us:
-    decfsz D1, f, c
-    bra L_us
+Lus: decfsz D1, f, c
+    bra Lus
     decfsz D2, f, c
-    bra L_us
+    bra Lus
     return
 
 Delay_Rest:
-    movlw 0x22
+    movlw 0x15           ; Faster loop for higher resolution skipping
     movwf D3, c
 DR1: movlw 0xFF
     movwf D2, c
@@ -138,7 +133,7 @@ LSM9DS1_Wakeup:
     bcf LATD, 0, b
     movlw 0x20
     call SPI_Xchg
-    movlw 0x60           
+    movlw 0x60
     call SPI_Xchg
     banksel LATD
     bsf LATD, 0, b
@@ -147,7 +142,7 @@ LSM9DS1_Wakeup:
 LSM9DS1_ReadX:
     banksel LATD
     bcf LATD, 0, b
-    movlw 0xA9           
+    movlw 0xA9
     call SPI_Xchg
     movlw 0x00
     call SPI_Xchg
@@ -158,8 +153,8 @@ LSM9DS1_ReadX:
 
 SPI_Xchg:
     movwf SSP2BUF, c
-W_S: btfss SSP2STAT, 0, c
-    bra W_S
+WS: btfss SSP2STAT, 0, c
+    bra WS
     movf SSP2BUF, W, c
     return
 
